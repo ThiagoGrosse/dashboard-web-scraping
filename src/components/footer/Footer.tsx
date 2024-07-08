2b24dc8d53e4395ae63a37bbb07f822ca5f21d01
Thiago Grosse <thi_g@live.com>
2024-07-08 16:40:09 -0300

    Ajuste rodapé

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     const version = process.env.VERSION;
 
     return (
-        <footer className="fixed bottom-0 bg-gray-300 w-full h-14 lg:h-10 flex justify-center items-center">
+        <footer className="fixed bottom-0 bg-gray-300 w-full h-14 lg:h-10 flex justify-center items-center z-50">
             <div className="text-xs flex flex-col lg:flex-row lg:gap-4 justify-center items-center opacity-80">
                 <span>&copy; 2024 Thiago Grosse. Todos os direitos reservados.</span>
                 <span>Versão: {version}</span>
